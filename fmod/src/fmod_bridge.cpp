c92496e27ae28476d36f2a74f444479b515d9516
Marius Petcu <dapetcu21@gmail.com>
2017-12-16 21:13:46 +0200

    Add LuaBridge and basic example

--- fmod/src/fmod_bridge.cpp
+++ fmod/src/fmod_bridge.cpp
@@ -1,23 +1,62 @@
 #include <fmod_errors.h>
+#include <dmsdk/sdk.h>
+#include <LuaBridge/LuaBridge.h>
 
 #include "fmod_bridge.hpp"
+
 using namespace FMODBridge;
+using namespace luabridge;
 
 FMOD::Studio::System* FMODBridge::system = NULL;
 
-void FMODBridge::init(lua_State *L) {
-    printf("Initializing FMOD with lua state %p\n", L);
+namespace FMODBridge {
+    void registerClasses(lua_State *L);
+}
 
-    FMOD_RESULT res = FMOD::Studio::System::create(&system);
+void FMODBridge::registerClasses(lua_State *L) {
+    getGlobalNamespace(L)
+        .beginNamespace("fmod")
+            .beginClass<FMOD::System>("System")
+            .endClass()
+            .beginNamespace("studio")
+                .beginClass<FMOD::Studio::System>("System")
+                .endClass()
+            .endNamespace()
+        .endNamespace();
+}
+
+void FMODBridge::init(lua_State *L) {
+    FMOD_RESULT res;
+
+    res = FMOD::Studio::System::create(&system);
     if (res != FMOD_OK) {
         printf("FMOD Error: %s\n", FMOD_ErrorString(res));
         system = NULL;
+        return;
+    }
+
+    registerClasses(L);
+
+    getGlobalNamespace(L)
+        .beginNamespace("fmod")
+            .beginNamespace("studio")
+                .addVariable("system", &system, false)
+            .endNamespace()
+        .endNamespace();
+
+    FMOD::System* lowLevelSystem;
+    res = system->getLowLevelSystem(&lowLevelSystem);
+    if (res != FMOD_OK) {
+        printf("FMOD Error: %s\n", FMOD_ErrorString(res));
+    } else {
+        getGlobalNamespace(L)
+            .beginNamespace("fmod")
+                .addVariable("system", &lowLevelSystem, false)
+            .endNamespace();
     }
 }
 
 void FMODBridge::finalize() {
-    printf("Finalizing FMOD\n");
-
     if (system) {
         FMOD_RESULT res = system->release();
         if (res != FMOD_OK) { printf("FMOD Error: %s\n", FMOD_ErrorString(res)); }
