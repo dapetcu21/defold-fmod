9f737aa360af0219e1d9f6648a8049fe1771d45b
Marius Petcu <dapetcu21@gmail.com>
2017-12-16 16:47:09 +0200

    Add FMOD libraries for macOS

--- fmod/src/fmod_bridge.cpp
+++ fmod/src/fmod_bridge.cpp
@@ -1,7 +1,15 @@
+#include <fmod_studio.hpp>
+#include <fmod.hpp>
+#include <fmod_errors.h>
+
 #include "fmod_bridge.hpp"
 
 void FMODBridge::init(lua_State *L) {
     printf("Initializing FMOD with lua state %p\n", L);
+
+    FMOD::Studio::System* system = NULL;
+    FMOD_RESULT res = FMOD::Studio::System::create(&system);
+    if (res != FMOD_OK) { printf("%s", FMOD_ErrorString(res)); }
 }
 
 void FMODBridge::finalize() {
