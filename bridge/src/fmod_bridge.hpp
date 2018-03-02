d027e6fd8ee83cabcccbb479609a73fc87daebc1
Marius Petcu <dapetcu21@gmail.com>
2018-03-02 20:51:44 +0200

    Fix logging

--- bridge/src/fmod_bridge.hpp
+++ bridge/src/fmod_bridge.hpp
@@ -30,14 +30,26 @@ extern "C" {
     #endif
 #endif
 
-#define LOGI(s, ...) ((void)printf("INFO:fmod: " s "\n", __VA_ARGS__))
-#define LOGE(s, ...) ((void)printf("ERROR:fmod: " s "\n", __VA_ARGS__))
-#define LOGW(s, ...) ((void)printf("WARNING:fmod: " s "\n", __VA_ARGS__))
+#define LOGI(...) ((void)printf("INFO:fmod: " FIRST(__VA_ARGS__) "\n" REST(__VA_ARGS__)))
+#define LOGE(...) ((void)printf("ERROR:fmod: " FIRST(__VA_ARGS__) "\n" REST(__VA_ARGS__)))
+#define LOGW(...) ((void)printf("WARNING:fmod: " FIRST(__VA_ARGS__) "\n" REST(__VA_ARGS__)))
 
 #define STRINGIFY(x) #x
 #define RESOLVE(x) x
 #define CONCAT_(a, b) a ## b
 #define CONCAT(a, b) CONCAT_(a, b)
+
+#define FIRST(...) FIRST_HELPER(__VA_ARGS__, throwaway)
+#define FIRST_HELPER(first, ...) first
+#define REST(...) REST_HELPER(NUM(__VA_ARGS__), __VA_ARGS__)
+#define REST_HELPER(qty, ...) REST_HELPER2(qty, __VA_ARGS__)
+#define REST_HELPER2(qty, ...) REST_HELPER_##qty(__VA_ARGS__)
+#define REST_HELPER_ONE(first)
+#define REST_HELPER_TWOORMORE(first, ...) , __VA_ARGS__
+#define NUM(...) \
+    SELECT_10TH(__VA_ARGS__, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE,\
+                TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, ONE, throwaway)
+#define SELECT_10TH(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
 
 extern "C" {
     typedef unsigned int FMODBridge_HBuffer;
